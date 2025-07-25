87cb7133c4e9bf35ddcb41c2b86cfff33a8b372d
MaxwellJChen <maxwelljchen1@gmail.com>
2025-07-24 20:22:12 -0700

    tweaked colors and appearance for default config

---
+++ convert.py
@@ -0,0 +1,2 @@
+colors = [48,92,222]
+print([color / 255 for color in colors])
--- src/render/shaders/floor.fs
+++ src/render/shaders/floor.fs
@@ -8,9 +8,9 @@ in vec3 FragPos;
 
 void main()
 {
-  vec3 color = vec3(0.8, 0.8, 0.8);
-  vec3 lightPos = vec3(5.0, 5.0, 5.0);
-  vec3 lightColor = vec3(1.0, 1.0, 1.0);
+  vec3 color = vec3(0.8);
+  vec3 lightPos = vec3(0.0, 10.0, 0.0);
+  vec3 lightColor = vec3(1.0);
   float ambientStrength = 0.25;
   vec3 ambient = ambientStrength * lightColor;
 
@@ -19,11 +19,13 @@ void main()
   float diff = max(dot(norm, lightDir), 0.0);
   vec3 diffuse = diff * lightColor;
 
-  float specularStrength = 0.7;
-  vec3 viewDir = normalize(viewPos - FragPos);
-  vec3 reflectDir = reflect(-lightDir, norm);
-  float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-  vec3 specular = specularStrength * spec * lightColor;
-  vec3 result = (ambient + diffuse + specular) * color;
+  vec3 result = (ambient + diffuse) * color;
+
+  // float specularStrength = 0.7;
+  // vec3 viewDir = normalize(viewPos - FragPos);
+  // vec3 reflectDir = reflect(-lightDir, norm);
+  // float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+  // vec3 specular = specularStrength * spec * lightColor;
+  // vec3 result = (ambient + diffuse + specular) * color;
   FragColor = vec4(result, 1.0);
 }
