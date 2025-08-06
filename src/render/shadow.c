f460e3db8b52738d2399bc9410a4120b65d03963
MaxwellJChen <maxwelljchen1@gmail.com>
2025-08-05 17:25:46 -0700

    feat: added PCF and fixed shadow stuttering

--- src/render/shadow.c
+++ src/render/shadow.c
@@ -7,8 +7,8 @@ unsigned int shadowInit(Shadow* s, Camera* c, vec3 lightDir)
   s->padding = 10.0f;
   glm_vec3_copy(lightDir, s->lightDir);
   glm_vec3_scale(s->lightDir, -s->dist, s->lightDelta);
-  s->SHADOW_WIDTH = 2048;
-  s->SHADOW_HEIGHT = 2048;
+  s->SHADOW_WIDTH = 4096;
+  s->SHADOW_HEIGHT = 4096;
   shaderInit(&s->shader, "../src/render/shaders/shadow.vs", "../src/render/shaders/shadow.fs");
 
   glGenFramebuffers(1, &s->FBO);
@@ -16,11 +16,14 @@ unsigned int shadowInit(Shadow* s, Camera* c, vec3 lightDir)
 
   glGenTextures(1, &s->depthMap);
   glBindTexture(GL_TEXTURE_2D, s->depthMap);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32F, s->SHADOW_WIDTH, s->SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24, s->SHADOW_WIDTH, s->SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
 
   glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, s->depthMap, 0);
 
@@ -64,27 +67,44 @@ void shadowUpdate(Shadow* s, Camera* c)
   }
 
   // create orthographic projection matrix
+  vec3 curMin;
+  vec3 curMax;
   for(int i = 0; i < 8; i++)
   {
     glm_mat4_mulv3(s->view, s->corners[i], 1.0f, s->corners[i]);
 
     if(i == 0)
     {
-      glm_vec3_copy(s->corners[i], s->min);
-      glm_vec3_copy(s->corners[i], s->max);
+      glm_vec3_copy(s->corners[i], curMin);
+      glm_vec3_copy(s->corners[i], curMax);
     }
     else
     {
-      glm_vec3_minv(s->corners[i], s->min, s->min);
-      glm_vec3_maxv(s->corners[i], s->max, s->max);
+      glm_vec3_minv(s->corners[i], curMin, curMin);
+      glm_vec3_maxv(s->corners[i], curMax, curMax);
     }
   }
+  unsigned int replace = 0;
   for(int i = 0; i < 3; i++)
   {
-    s->min[i] -= s->padding;
-    s->max[i] += s->padding;
+    if(curMin[i] < s->min[i] || curMax[i] > s->max[i])
+    {
+      replace = 1;
+      break;
+    }
   }
-  glm_ortho(s->min[0], s->max[0], s->min[1], s->max[1], -s->max[2], -s->min[2], s->projection);
+
+  if(replace)
+  {
+    glm_vec3_copy(curMin, s->min);
+    glm_vec3_copy(curMax, s->max);
+    for(int i = 0; i < 3; i++)
+    {
+      s->min[i] -= s->padding;
+      s->max[i] += s->padding;
+    }
+    glm_ortho(s->min[0], s->max[0], s->min[1], s->max[1], -s->max[2], -s->min[2], s->projection);
+  }
 
   // create vp matrix
   glm_mat4_mul(s->projection, s->view, s->vp);
